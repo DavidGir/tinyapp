d44d75046dc89816b6b9c51bc28d492243a12b1f
David Giroux <DavidGiroux149@gmail.com>
2023-11-01 11:41:30 -0400

    testing new /login cookie

--- express_server.js
+++ express_server.js
@@ -61,6 +61,7 @@ app.post("/urls", (req, res) => {
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
+  console.log("req.cookies", req.cookies); // test
   res.redirect("/urls");
 });
 
