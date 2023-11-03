f019cba299fbde55173fd1d099a1077b19db29ef
David Giroux <DavidGiroux149@gmail.com>
2023-11-03 01:18:46 -0400

    Took out helper functions from server file

--- express_server.js
+++ express_server.js
@@ -59,54 +59,6 @@ app.use(cookieSession({
 // Use the logger middleware:
 app.use(morgan("dev"));
 
-
-// UTILITY FUNCTIONS:
-
-//generateRandomString function; generates random string to be used as a short URL identifier:
-// const generateRandomString = function() {
-//   // Initialize variable to generate random six alphanumeric characters:
-//   const randomString = Math.random().toString(36).substring(2, 8);
-//   // Return result:
-//   return randomString;
-// };
-
-// // Helper function to handle registration errors:
-// const findUserByEmail = (email, users) => {
-//   // Loop through users object:
-//   for (const userID in users) {
-//     const user = users[userID];
-//     // If user email is in our users data store (exists):
-//     if (user.email === email) {
-//       // Return user object for match found:
-//       return user;
-//     }
-//   }
-//   // Return null if no match exists:
-//   return null;
-// };
-
-// // Helper function to filter the urlDatabase for URLs that belong to the user:
-// const urlsForUser = (id) => {
-//   // Initialize empty user URLs object:
-//   const userURLs = {};
-//   // Loop through urlDatabase object:
-//   for (const urlId in urlDatabase) {
-//     // If current user's ID exists in the urlDatabase:
-//     if (urlDatabase[urlId].userID === id) {
-//       userURLs[urlId] = urlDatabase[urlId];
-//     }
-//   }
-//   // Return user URLs:
-//   return userURLs;
-// };
-
-// // Helper function to check if a given URL belongs to a user:
-// const urlBelongsToUser = (shortURL, userID) => {
-//   const urlObject = urlDatabase[shortURL];
-//   // Returns true if URL belongs to the user and false otherwise:
-//   return urlObject && urlObject.userID === userID;
-// };
-
 // --------------------------------------------------------------------------
 
 // ROUTE METHODS:
